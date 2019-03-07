6a2719f48d19a2939ef883fa06c0b9a9f7b9727d
rbuzatto <mightyrafa@gmail.com>
2019-03-07 00:34:37 -0300

    update locall env and local database

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ module.exports = {
     SpotifyAuth: process.env.SPOTIFY_AUTH,
     secretKey: process.env.SECRET_KEY,
     dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD
+    dbPassword: process.env.DB_PASSWORD,
+    env: process.env.NODE_ENV
 }
--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const keys = require('../config/keys')
 
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb://${keys.dbUser}:${keys.dbPassword}@ds349175.mlab.com:49175/spotify`, {
+
+const url = keys.env === 'development' 
+    ? 'mongodb://localhost:27017/spotify'
+    : `mongodb://${keys.dbUser}:${keys.dbPassword}@ds349175.mlab.com:49175/spotify`
+mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
